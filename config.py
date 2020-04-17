74949dbd12215470683715db5e5d4697c2bd038e
kris <kriswucn@hotmail.com>
2020-04-17 14:24:22 +0800

    监听端口使用配置文件

--- config.py
+++ config.py
@@ -5,4 +5,5 @@ BASE_DIR = os.path.abspath(os.path.dirname(__name__))
 UPLOADS_DIR = 'app/static/uploads'
 CSV_DIR = 'app/static/cvs'
 LOG_DIR = 'logs'
+PORT = 2333
 
--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 from app import app
 from app.utils import logging_utils as lu
 import logging
+import config
 
 logger = lu.Logger(__name__, cmd_level=logging.INFO, file_level=logging.INFO)
 
 if __name__ == '__main__':
-    logger.logger.info('Application started')
-    app.run('0.0.0.0', port=8000)
+    logger.logger.info('---------- Application started ----------')
+    app.run('0.0.0.0', port=config.PORT)
